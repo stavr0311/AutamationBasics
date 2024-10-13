4e5591077c2a0f1fde1cba7bb8a3ec2ef383bc78
Stavr <rommman@inbox.ru>
2024-10-13 15:26:59 +0300

    CashbackHackService

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void Purchasebonuses() {
+        CashbackHackService CashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = CashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+}
